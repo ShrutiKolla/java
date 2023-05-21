958a1ed0f3617f9c7511311af08b7b5669e002b3
me-mystic <Shrutikolla.2002@gmail.com>
2023-05-21 21:48:51 +0530

    .

--- 33-BST1/BST.java
+++ 33-BST1/BST.java
@@ -9,7 +9,16 @@ public class BST {
             right = null;
         }
     }
+    public static void inorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
 
+    // 1. insert a node
     public static Node insert(Node root, int val) {
         if(root == null) {
             root = new Node(val);
@@ -22,22 +31,79 @@ public class BST {
         } 
         return root;
     }
-    public static void inorder(Node root) {
+
+    // 2. search a node
+    public static boolean search(Node root, int val) {
+        if(root == null) {
+            return false;
+        }
+        if(root.data == val) {
+            return true;
+        }
+        if(val < root.data) {
+            return search(root.left, val);
+        } else {
+            return search(root.right, val);
+        }
+    }
+
+    // 3. delete a node
+
+    public static int InorderSuccessor(Node root) {
+        while(root.left != null) {
+            root = root.left;
+        }
+        return root.data;
+    }
+    public static Node delete(Node root, int val) {
+        if(val < root.data) {
+            root.left = delete(root.left, val);
+        } else if (val > root.data) {
+            root.right = delete(root.right, val);
+        } else {
+            if(root.left == null && root.right == null) {
+                return null;
+            }
+            if(root.left == null) {
+                return root.right;
+            } else if( root.right == null) {
+                return root.left;
+            } else {
+                int IS = InorderSuccessor(root.right);
+                root.data = IS;
+                root.right = delete(root.right, IS);
+            }
+        }
+        return root;
+    }
+
+    // 4. print in range
+    public static void printIR(Node root, int k1, int k2) {
         if(root == null) {
             return;
         }
-        inorder(root.left);
-        System.out.print(root.data + " ");
-        inorder(root.right);
+        if(root.data >= k1 && root.data <= k2) {
+            printIR(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printIR(root.right, k1, k2);
+        } else if(root.data < k1) {
+            printIR(root.right, k1, k2);
+        } else {
+            printIR(root.left, k1, k2);
+        }
     }
-
     public static void main(String[] args) {
-        int values[] = {5, 1, 3, 4, 2, 7};
+        int values[] = {8,6,3,5,1,4,10,11,14};
+        // int values[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
         }
+        // System.out.println(search(root, 0));
+        // delete(root, 5);
         inorder(root);
+        System.out.println();
+        printIR(root, 5, 12);
     }
     
 }
