e92eee9d2f89d9f6ad1cf580b03b8dd54105c347
me-mystic <Shrutikolla.2002@gmail.com>
2023-06-05 00:09:18 +0530

    .

--- 35-heaps/Heaps.java
+++ 35-heaps/Heaps.java
@@ -58,6 +58,52 @@ public class Heaps {
                 heapify(minIdx);
             }
         }
+
+        // 1. heap sort
+
+        private void Heapify(int idx, int size, int[] arr) {
+            int left = idx * 2 + 1;
+            int right = idx * 2 + 2;
+            int maxIdx = idx;
+
+            if(left < size && arr[maxIdx] < arr[left]) {
+                maxIdx = left;
+            }
+
+            if(right < size && arr[maxIdx] < arr[right]) {
+                maxIdx = right;
+            }
+
+            if(maxIdx != idx) {
+                // swap
+                int temp = arr[maxIdx];
+                arr[maxIdx] = arr[0];
+                arr[0] = temp;
+                Heapify(maxIdx, size, arr);
+            }
+        }
+        public void heapSort(int[] arr) {
+            // arr -> maxHeap
+            int n = arr.length;
+            for (int i = n/2; i >= 0; i--) {
+                Heapify(i, n, arr);
+            }
+
+            /*
+                2. 
+                    swap(0,i)
+                    heapify(0,i)
+            */ 
+
+            for (int i = n - 1; i >= 0; i--) {
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[0];
+                arr[0] = temp;
+                Heapify(0, i, arr);
+            }
+        }
+
         public void print() {
             for (int i = 0; i < AL.size(); i++) {
                 System.out.print(AL.get(i) + " ");
@@ -66,16 +112,27 @@ public class Heaps {
     }
 
     public static void main(String[] args) {
-        heap h1 = new heap();
-        h1.insert(2);
-        h1.insert(3);
-        h1.insert(4);
-        h1.insert(5);
-        h1.insert(10);
-        h1.insert(1);
-        h1.print();
-        h1.delete();
-        System.out.println();
-        h1.print();
+
+        // heapSort
+        int arr[] = {1, 2, 4, 5, 3};
+        heap h2 = new heap();
+        h2.heapSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        // delete
+        // heap h1 = new heap();
+        // h1.insert(2);
+        // h1.insert(3);
+        // h1.insert(4);
+        // h1.insert(5);
+        // h1.insert(10);
+        // h1.insert(1);
+        // h1.print();
+        // h1.delete();
+        // System.out.println();
+        // h1.print();
+
+
     }
 }
