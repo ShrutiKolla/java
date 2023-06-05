e15d18e76d3fe93b0576a94e48fc5cd0363bb7eb
me-mystic <Shrutikolla.2002@gmail.com>
2023-06-05 11:57:08 +0530

    .

--- 35-heaps/Heaps.java
+++ 35-heaps/Heaps.java
@@ -103,7 +103,8 @@ public class Heaps {
                 Heapify(0, i, arr);
             }
         }
-
+        
+        
         public void print() {
             for (int i = 0; i < AL.size(); i++) {
                 System.out.print(AL.get(i) + " ");
@@ -111,15 +112,96 @@ public class Heaps {
         }
     }
 
+    // 2. Nearest k cars
+
+    static class Dist implements Comparable<Dist>{
+        int x, y;
+        String name;
+        int distSq;
+
+        Dist(int i, int x, int y) {
+            this.name = "C" + i;
+            this.x = x;
+            this.y = y;
+            distSq = (int)Math.pow(x, 2) + (int)Math.pow(y, 2);
+        }
+        @Override
+        public int compareTo(Dist o) {
+            if(this.distSq == o.distSq) {
+                return 0;
+            }
+
+            return this.distSq < o.distSq ? -1 : 1;
+        }
+
+        @Override
+        public String toString() {
+            return name + ": " + Math.sqrt(distSq) + " cm sq. ";
+        }
+
+    }
+    public static void nearestKcars(int cars[][], int k) {
+        PriorityQueue<Dist> PQ = new PriorityQueue<>();
+        for (int i = 0; i < cars.length; i++) {
+            PQ.add(new Dist(i, cars[i][0], cars[i][1]));
+        }
+        for (int i = 0, a = 1; i < PQ.size() && a <= k; i++, a++) {
+            System.out.println(PQ.remove());
+        }
+
+    }
+
+    // 3. connect N ropes
+    static class Ropes implements Comparable<Ropes>{
+        int rlen;
+        Ropes(int r) {
+            rlen = r;
+        }
+
+        public int compareTo(Ropes r) {
+            if(this.rlen == r.rlen) {
+                return 0;
+            }
+            return this.rlen < r.rlen ? -1 : 1;
+        }
+        public String toString() {
+            return "len: " + rlen;
+        }
+    }
+    public static void connectNropes(int ropes[]) {
+        int cost = 0;
+
+        PriorityQueue<Ropes> PQ = new PriorityQueue<>();
+        for (int i = 0; i < ropes.length; i++) {
+            PQ.add(new Ropes(ropes[i]));
+        }
+
+        while(PQ.size() > 1) {
+            int sum = PQ.remove().rlen + PQ.remove().rlen;
+            PQ.add(new Ropes(sum));
+            cost += sum;
+        }
+        System.out.println("total cost : " + cost);
+    }
+
     public static void main(String[] args) {
 
-        // heapSort
-        int arr[] = {1, 2, 4, 5, 3};
-        heap h2 = new heap();
-        h2.heapSort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        // 3.
+        int ropes[] = {2,3,4,6};
+        connectNropes(ropes);
+
+        // 2. nearest k cars
+        // int cars[][] = {{3,3}, {5,-1}, {-2, 4}};
+        // nearestKcars(cars, 2);
+
+        // 1. heapSort
+        // int arr[] = {1, 2, 4, 5, 3};
+        // heap h2 = new heap();
+        // h2.heapSort(arr);
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
+
         // delete
         // heap h1 = new heap();
         // h1.insert(2);
