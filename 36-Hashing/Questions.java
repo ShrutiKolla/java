aba71da8a862fe5811040b279e08d638e96de2ec
me-mystic <Shrutikolla.2002@gmail.com>
2023-06-13 23:06:30 +0530

    .

--- 36-Hashing/Questions.java
+++ 36-Hashing/Questions.java
@@ -99,7 +99,7 @@ public class Questions {
         HashMap<String, String> revMap = new HashMap<>();
         for(String key : tickets.keySet()) {
             revMap.put(tickets.get(key), key);
-        }
+        };
         for(String k : tickets.keySet()) {
             if(!revMap.containsKey(k)) {
                 return k;
@@ -107,14 +107,48 @@ public class Questions {
         }
         return null;
     }
+
+    // 6. largest subarray sum is 0
+    public static void LargestSubarray0(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int len = 0;
+        for (int j = 0; j < arr.length; j++) {
+            sum += arr[j];
+            if(map.containsKey(sum)) {
+                len =Math.max(len, j - map.get(sum));
+            } else {
+                map.put(sum, j);
+            }
+        }
+        System.out.println("largest: " + len);
+    }
+    // subarray sum = k
+    public static void subarrSumK(Integer arr[], int k) {
+        HashMap<Integer, Integer> map = new HashMap<>(); // (sum, count)
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if(sum == k) {
+                if(map.containsKey(sum)) {
+
+                }
+            }            
+        }
+    }
     public static void main(String[] args) {
+        //6. Largest subarray sum is 0
+        // int arr[] = {15, -2, 2, -8, 1, 7, 10};
+        int arr[] = {3,4,5};
+        LargestSubarray0(arr); 
+
         // 5. find itinerary from tickets
-        HashMap<String, String> tickets = new HashMap<>();
-        tickets.put("Chennai", "Bengaluru"); 
-        tickets.put("Mumbai", "Delhi"); 
-        tickets.put("Goa", "Chennai"); 
-        tickets.put("Delhi", "Goa"); 
-        itinerary(tickets);
+        // HashMap<String, String> tickets = new HashMap<>();
+        // tickets.put("Chennai", "Bengaluru"); 
+        // tickets.put("Mumbai", "Delhi"); 
+        // tickets.put("Goa", "Chennai"); 
+        // tickets.put("Delhi", "Goa"); 
+        // itinerary(tickets);
 
         // 4. union and intersection
         // int arr1[] = {7,3,9};
