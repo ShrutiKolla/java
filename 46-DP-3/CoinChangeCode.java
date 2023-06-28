3191ae66dc785f6ed20cca23778d620a02376969
me-mystic <Shrutikolla.2002@gmail.com>
2023-06-28 14:18:11 +0530

    .

--- 46-DP-3/CoinChangeCode.java
+++ 46-DP-3/CoinChangeCode.java
@@ -1,25 +1,27 @@
 public class CoinChangeCode {
     public static void coinChange(int[] coins, int sum) {
         int n = coins.length;
-        boolean dp[][] = new boolean[n+1][sum+1];
+        int dp[][] = new int[n+1][sum+1];
 
-        // initialize 1st col with 0 and 1st row except 0th index with 0
+        // initialize 1st col with 1 and 1st row except 0th index with 0
         for (int i = 0; i < dp.length; i++) {
-            dp[i][0] = true;
+            dp[i][0] = 1;
         }
-        int ways = 0;
-        for (int i = 0; i < dp.length + 1; i++) {
-            for (int j = 0; j < sum + 1; j++) {
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
                 int w = coins[i-1];
                 if(w <= j) {
-                    if(dp)
+                    dp[i][j] = dp[i][j - w] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
+        System.out.println(dp[n][sum]);
     }
     public static void main(String[] args) {
-        int[] coins = {1, 2, 3};
-        int sum = 4;
+        int[] coins = {2, 5, 3, 6};
+        int sum = 10;
         coinChange(coins, sum);
     }    
 }
