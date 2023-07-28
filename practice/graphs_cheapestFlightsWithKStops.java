98850dd58ea863de98ddb5466a74a9f56c01347c
me-mystic <Shrutikolla.2002@gmail.com>
2023-07-28 22:29:54 +0530

    .

---
+++ practice/graphs_cheapestFlightsWithKStops.java
@@ -0,0 +1,14 @@
+package practice;
+import java.util.*;
+public class graphs_cheapestFlightsWithKStops {
+    static class Edge {
+        int src,dest,wt;
+    }
+
+    public static void create(ArrayList<Edge>[] graph) {
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
