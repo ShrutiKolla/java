148ccbf9415034fa26d3949343aca91211bebc52
me-mystic <Shrutikolla.2002@gmail.com>
2023-07-29 15:53:00 +0530

    .

--- practice/graphs_cheapestFlightsWithKStops.java
+++ practice/graphs_cheapestFlightsWithKStops.java
@@ -1,4 +1,3 @@
-package practice;
 
 import java.util.*;
 
@@ -17,11 +16,15 @@ public class graphs_cheapestFlightsWithKStops {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0, 1, 100));
-        graph[1].add(new Edge(1, 2, 100));
-        graph[2].add(new Edge(2, 0, 100));
-        graph[1].add(new Edge(1, 3, 600));
-        graph[2].add(new Edge(2, 3, 200));
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        // graph[0].add(new Edge(0, 1, 100));
+        // graph[1].add(new Edge(1, 2, 100));
+        // graph[2].add(new Edge(2, 0, 100));
+        // graph[1].add(new Edge(1, 3, 600));
+        // graph[2].add(new Edge(2, 3, 200));
     }
 
     public static class Pair {
@@ -34,7 +37,7 @@ public class graphs_cheapestFlightsWithKStops {
         }
     }
 
-    public static void cheapFlight(ArrayList<Edge>[] graph, int src, int dest, int k) {
+    public static int cheapFlight(ArrayList<Edge>[] graph, int src, int dest, int k) {
         Queue<Pair> q = new LinkedList<>();
         boolean vis[] = new boolean[graph.length];
         int[] dist = new int[graph.length];
@@ -50,18 +53,29 @@ public class graphs_cheapestFlightsWithKStops {
             if (curr.stops > k) {
                 break;
             }
-            if (!vis[curr.v]) {
-                for (int i = 0; i < graph[curr.v].size(); i++) {
-                    Edge e = graph[curr.v].get(i);
-                }
+            for (int i = 0; i < graph[curr.v].size(); i++) {
+                Edge e = graph[curr.v].get(i);
+                int s = e.src;
+                int d = e.dest;
+                int c = e.cost;
+                //relaxation
+                if(dist[d] > curr.cost + c) {
+                    dist[d] = curr.cost + c;
+                    q.add(new Pair(d, dist[d], curr.stops+1));
+                } 
             }
-
         }
+
+        if(dist[dest] != Integer.MAX_VALUE) {
+            return dist[dest];
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
         @SuppressWarnings("unchecked")
-        ArrayList<Edge>[] graph = new ArrayList[5];
+        ArrayList<Edge>[] graph = new ArrayList[4];
         create(graph);
+        System.out.println(cheapFlight(graph, 0, 3, 1));
     }
 }
